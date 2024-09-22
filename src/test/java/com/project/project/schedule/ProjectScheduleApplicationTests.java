ac6e7d6bdbda78ef2222bce2c82c7c621f731c4a
venu2432 <venumaripi765@gmail.com>
2024-09-22 19:55:22 +0530

    Base package

---
+++ src/test/java/com/project/project/schedule/ProjectScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.project.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
